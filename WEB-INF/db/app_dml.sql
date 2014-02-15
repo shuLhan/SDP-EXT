e733c4e438409f65e281df9be9e33cd0813e8a02
Mhd Sulhan <m.shulhan@gmail.com>
2014-02-15 10:24:08 +0700

    [SDP-EXT] Add module shortcut icon to the Home page.

--- WEB-INF/db/app_dml.sql
+++ WEB-INF/db/app_dml.sql
@@ -1,7 +1,11 @@
 INSERT INTO rutanbandung_db._menu VALUES (3, 0, 0, 'Telepon'	, 'telephone'	, '', 'MainTelephone', '');
 INSERT INTO rutanbandung_db._menu VALUES (4, 3, 1, 'Manajemen'	, 'manage'		, '', 'Telephone', '');
 INSERT INTO rutanbandung_db._menu VALUES (5, 3, 1, 'Riwayat'	, 'history'		, '', 'Telephone_History', '');
+INSERT INTO rutanbandung_db._menu VALUES (100, 1, 2, 'Manajemen Telepon', '', 'Telephone_Manage', 'Telephone', '');
+INSERT INTO rutanbandung_db._menu VALUES (101, 1, 2, 'Riwayat Telepon', '', 'Telephone_History', 'Telephone_History', '');
 
 INSERT INTO rutanbandung_db._group_menu VALUES (1, 3, 4);
 INSERT INTO rutanbandung_db._group_menu VALUES (1, 4, 4);
 INSERT INTO rutanbandung_db._group_menu VALUES (1, 5, 4);
+INSERT INTO rutanbandung_db._group_menu VALUES (1, 100, 4);
+INSERT INTO rutanbandung_db._group_menu VALUES (1, 101, 4);
